d31b52d17cdea189f554e0b01ddcfa265693835b
Shaimall <sshaimall1738@gmail.com>
2022-10-29 05:00:28 -0700

    3rd commit

--- components/P1.jsx
+++ components/P1.jsx
@@ -245,7 +245,7 @@ const P1 = () => {
                   Sustainable
                   <br></br> keytar Tumblr, authentic<br></br>mustache locavore
                   scenester<br></br> gentrify meh banjo fanny pack<br></br>{" "}
-                  health goth. Four dollar toas
+                  health oth. Four dollar toas
                 </p>
               </div>
             </div>
