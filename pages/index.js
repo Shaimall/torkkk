47a87bf8bf7e9a656c8c2989fa384079f5f848f3
Shaimall <sshaimall1738@gmail.com>
2022-10-30 02:41:13 -0700

    Fixed Issues

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import React from "react";
+import { Card } from "../components/card";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       {/* Remove this*/}
 
       {/* the header can be of min hieght screen and have a backgroung image of the hero image */}
-      <header className="min-h-screen bg-no-repeat  bg-[url('/img/p1.jpg')] ">
+      <header className="min-h-screen bg-cover bg-no-repeat bg- bg-[url('/img/p1.jpg')] ">
         {/* the logo can go here in div. it can be aligned left with layout any position method */}
 
         <div>
@@ -16,46 +17,44 @@ export default function Home() {
         </div>
       </header>
 
-      <section className=" bg-[#835FA8]">
+      <section className=" bg-[#835FA8] md:h-[500px] ">
         {/* you can use a grid to sperate the two sections */}
-        <div className="grid grid-cols-2 max-w-5xl mx-auto pt-24 px-10">
-          <div className="">
+        <div className="grid sm:grid-cols-2 max-w-full md:h-[500px] sm:container ">
+          <div className=" relative sm:order-last   ">
+            {/*2nd COL the image can go here and you can you position absolute to position them. its easier if you export the two phone images togeter */}
+
+            <img
+              className="sm:absolute bottom-0 right-0 md:h-[110%] sm:max-w-none  "
+              src="img/phones2.png"
+              alt=""
+            />
+          </div>
+          <div className=" sm:w-auto pl-3 pt-10 ">
             {/* 1st COL the Heor feature text and the button can go here */}
-            <p className=" font-openl text-white text-[48px] ">
+            <p className=" font-openl text-white text-xl sm:text-[48px] sm:pb-14  ">
               Super Duper & such
             </p>
 
-            <p className="  font-openl text-white text-[18px]  ">
+            <p className="  font-openl text-white text-sm sm:text-[18px] sm:pb-4 sm:leading-8 md:pb-7">
               Lorem ipsum dolor sit amet, consectetur adipisicing<br></br> elit,
               sed do eiusmod tempor incididunt ut labore et<br></br> dolore
               magna. aliqua
             </p>
 
-            <div className="">
-              <button className="px-11 py-3 rounded-full bg-[#3DD2AE] mt-8 text-sm text-white  ">
+            <div className="h-12 pt-2">
+              <button className="px-2 sm:px-7 py-1 sm:py-3 rounded-full bg-[#3DD2AE] text-xs sm:text-l text-white   ">
                 See More
               </button>
             </div>
-          </div>
-          <div className="self-end">
-            {/*2nd COL the image can go here and you can you position absolute to position them. its easier if you export the two phone images togeter */}
-            <img src="img/phones.png" alt="" />
           </div>
         </div>
       </section>
 
       {/* Feature section */}
-      <section className="px-[160px] grid grid-cols-1 sm:grid-cols-3 gap-14 pt-[120px] pb-[100px]  ">
+      <section className="px-[10px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:gap-14 gap-7 pt-[120px] pb-[100px]  ">
         {/* use a grid to make 3 columns and make a CARD COMPONENT here since the html elements are being repeated. you can loop the array i have created at the bottom of the file*/}
         {FEATER_CARD.map((item) => (
-          <div className="">
-            <p className="text-[38px]">{item.title}</p>
-            <p className="text-[25px] pt-4">{item.exerp}</p>
-            <p className="text-[18px] pt-4">{item.description}</p>
-            <button className="px-16 py-4 rounded-full bg-[#3DD2AE] mt-11 text-[18px] text-white ">
-              See More
-            </button>
-          </div>
+          <Card {...item} />
         ))}
       </section>
     </div>
@@ -67,21 +66,21 @@ const FEATER_CARD = [
     title: "Design & Features",
     exerp: "Flannel ennui narwhal, craft beer twee Vice plaid authentic synth.",
     description:
-      "Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial",
+      "Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.",
     href: "#",
   },
   {
     title: "Pocket Power",
     exerp: "Flannel ennui narwhal, craft beer twee Vice plaid authentic synth.",
     description:
-      "Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial",
+      "Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.",
     href: "#",
   },
   {
     title: "Inspired by the world",
     exerp: "Flannel ennui narwhal, craft beer twee Vice plaid authentic synth.",
     description:
-      "Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial ",
+      "Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest. ",
     href: "#",
   },
 ];
