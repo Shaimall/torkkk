a689b7a54607b2ef61139b4113ac9881f085cdac
Shaimall <sshaimall1738@gmail.com>
2022-10-31 06:25:16 -0700

    Changes made

--- components/card.jsx
+++ components/card.jsx
@@ -2,13 +2,12 @@ import React from "react";
 
 export const Card = ({ title, exerp, description }) => {
   return (
-    <div className="flex flex-col">
-      <p className="text-[38px] h-28">{title}</p>
+    <div className="md:flex flex-wrap  text-center md:text-left p-4 py-12 ">
+      <p className="text-[38px] ">{title}</p>
       <p className="text-[25px] pt-4">{exerp}</p>
-      <p className="text-[18px] pt-4 mb-8 max-h-24 overflow-hidden">
-        {description}
-      </p>
-      <button className="px-16 py-4 rounded-full bg-[#3DD2AE] mt-auto text-[18px] text-white ">
+      <p className="text-[18px] pt-4 mb-8 ">{description}</p>
+
+      <button className="px-20 py-3 sm:py-3 rounded-full sm:px-9 bg-[#3DD2AE]  text-[18px] text-white md:self-end flex-auto md:flex-none">
         See More
       </button>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
 import React from "react";
 import { Card } from "../components/card";
 
@@ -9,53 +7,56 @@ export default function Home() {
       {/* Remove this*/}
 
       {/* the header can be of min hieght screen and have a backgroung image of the hero image */}
-      <header className="min-h-screen bg-cover bg-no-repeat bg- bg-[url('/img/p1.jpg')] ">
+      <header className="min-h-screen bg-no-repeat md:bg-cover bg-center  bg-[url('/img/p1.jpg')] ">
         {/* the logo can go here in div. it can be aligned left with layout any position method */}
 
-        <div>
-          <img className="w-[61px] h-[77px]" src="img/logo.svg" alt="" />
+        <div className="absolute left-10 flex flex-shrink mt-9">
+          <img className="w-full h-20 sm:h-32" src="img/logo.svg" alt="" />
         </div>
       </header>
 
       <section className=" bg-[#835FA8] md:h-[500px] ">
         {/* you can use a grid to sperate the two sections */}
-        <div className="grid sm:grid-cols-2 max-w-full md:h-[500px] sm:container ">
-          <div className=" relative sm:order-last   ">
-            {/*2nd COL the image can go here and you can you position absolute to position them. its easier if you export the two phone images togeter */}
-
-            <img
-              className="sm:absolute bottom-0 right-0 md:h-[110%] sm:max-w-none  "
-              src="img/phones2.png"
-              alt=""
-            />
-          </div>
-          <div className=" sm:w-auto pl-3 pt-10 ">
+        <div className="grid md:grid-cols-2  md:h-[500px] md:container xl:px-40 ">
+          <div className=" md:w-auto pl-3 pt-10 md:mt-20 ">
             {/* 1st COL the Heor feature text and the button can go here */}
-            <p className=" font-openl text-white text-xl sm:text-[48px] sm:pb-14  ">
+            <p className="text-center md:text-left font-openl text-white sm:text-5xl md:text-2xl xl:text-3xl mb-2 sm:leading-normal text-5xl ">
               Super Duper & such
             </p>
 
-            <p className="  font-openl text-white text-sm sm:text-[18px] sm:pb-4 sm:leading-8 md:pb-7">
+            <p className=" text-center md:text-left font-openl text-white md:text-lg  xl:text-xl md:pb-4 sm:leading-relaxed md:pb-7 text-2xl">
               Lorem ipsum dolor sit amet, consectetur adipisicing<br></br> elit,
               sed do eiusmod tempor incididunt ut labore et<br></br> dolore
               magna. aliqua
             </p>
 
-            <div className="h-12 pt-2">
-              <button className="px-2 sm:px-7 py-1 sm:py-3 rounded-full bg-[#3DD2AE] text-xs sm:text-l text-white   ">
+            <div className="h-12 sm:pt-1 text-center md:text-left ">
+              <button className="px-6 sm:px-7 py-3 sm:py-3 rounded-full bg-[#3DD2AE] text-xs sm:text-l text-white   ">
                 See More
               </button>
             </div>
+          </div>
+          <div className=" relative    ">
+            {/*2nd COL the image can go here and you can you position absolute to position them. its easier if you export the two phone images togeter */}
+
+            <img
+              className="md:absolute bottom-0 right-0 md:h-[110%] md:max-w-none   "
+              src="img/phones2.png"
+              alt=""
+            />
           </div>
         </div>
       </section>
 
       {/* Feature section */}
-      <section className="px-[10px] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:gap-14 gap-7 pt-[120px] pb-[100px]  ">
+
+      <section className=" bg-[#F4F8FB] ">
         {/* use a grid to make 3 columns and make a CARD COMPONENT here since the html elements are being repeated. you can loop the array i have created at the bottom of the file*/}
-        {FEATER_CARD.map((item) => (
-          <Card {...item} />
-        ))}
+        <div className="container px-20 py-14  grid grid-cols-1  md:grid-cols-3 md:gap-14 ">
+          {FEATER_CARD.map((item, i) => (
+            <Card {...item} key={i} />
+          ))}
+        </div>
       </section>
     </div>
   );
@@ -66,21 +67,21 @@ const FEATER_CARD = [
     title: "Design & Features",
     exerp: "Flannel ennui narwhal, craft beer twee Vice plaid authentic synth.",
     description:
-      "Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.",
+      "Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.",
     href: "#",
   },
   {
     title: "Pocket Power",
     exerp: "Flannel ennui narwhal, craft beer twee Vice plaid authentic synth.",
     description:
-      "Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.",
+      "Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.",
     href: "#",
   },
   {
     title: "Inspired by the world",
     exerp: "Flannel ennui narwhal, craft beer twee Vice plaid authentic synth.",
     description:
-      "Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest. ",
+      "Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog crucifix put a bird on it four dollar toast cardigan scenester aesthetic sartorial chia messenger bag Pinterest.Kickstarter umami Intelligentsia kogi. Try-hard flannel literally, chambray blog.",
     href: "#",
   },
 ];
