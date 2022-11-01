902a8ddd752421333c89abe801157734f372cb41
Shaimall <sshaimall1738@gmail.com>
2022-11-01 00:07:20 -0700

    Changes-FInal

--- components/card.jsx
+++ components/card.jsx
@@ -7,7 +7,7 @@ export const Card = ({ title, exerp, description }) => {
       <p className="text-lg pt-4">{exerp}</p>
       <p className="text-base pt-4 mb-8 ">{description}</p>
 
-      <button className="w-full px-20 py-3 sm:py-3  rounded-full sm:px-9 bg-[#3DD2AE]  text-xs text-white md:self-end flex-auto md:flex-none">
+      <button className="w-full px-4 py-3 sm:py-3  rounded-full sm:px-9 bg-[#3DD2AE]  text-xs text-white md:self-end flex-auto md:flex-none">
         See More
       </button>
     </div>
